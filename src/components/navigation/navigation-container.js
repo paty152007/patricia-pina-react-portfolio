9819e5383d3431dd07f69bae78c436eba758f997
mac <mac@macs-MacBook-Pro.local>
2026-09-09 20:45:36 +0200

    9 sept

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,8 +1,8 @@
-import React, { Component} from "react";
-import { NavLink } from "react-router-dom"; 
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 export default class NavigationComponent extends Component {
-    constructor () {
+    constructor() {
         super();
     }
 
@@ -10,21 +10,28 @@ export default class NavigationComponent extends Component {
         return (
             <div className="nav-wrapper">
                 <div className="left-side">
-                <NavLink exact to="/" activeClasName="nav-link-active"> 
-                Home 
-                </NavLink>
-                <NavLink  to="/about-me" activeClassName="nav-link-active">
-                About me
-                </NavLink>
-                <NavLink  to="/contact" activeClassName="nav-link-active">
-                Contact
-                </NavLink>
-                <NavLink  to="/blog" activeClassName="nav-link-active">
-                Blog
-                </NavLink>
-
-                {false ? <button>Add Blog</button> : null }
+                    <div className="nav-link-wrapper">
+                        <NavLink exact to="/" activeClassName="nav-link-active">
+                            Home
+                        </NavLink>
+                    </div>
+                    <div className="nav-link-wrapper">
+                        <NavLink to="/about-me" activeClassName="nav-link-active">
+                            About me
+                        </NavLink>
+                    </div>
+                    <div className="nav-link-wrapper">
+                        <NavLink to="/contact" activeClassName="nav-link-active">
+                            Contact
+                        </NavLink>
+                    </div>
+                    <div className="nav-link-wrapper">
+                        <NavLink to="/blog" activeClassName="nav-link-active">
+                            Blog
+                        </NavLink>
+                    </div>
                 </div>
+
                 <div className="right-side">
                     PATRICIA PIÑA
                 </div>
