3e19b505b502c32e053ef46ca6f9b8e6fc3cc0ca
mac <mac@macs-MacBook-Pro.local>
2026-09-17 11:32:42 +0200

    17 sept

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { withRouter } from "react-router";
 import { NavLink } from "react-router-dom";
 
@@ -61,8 +62,12 @@ const NavigationComponent = (props) => {
 
       <div className="right-side">
         PATRICIA PIÑA
-        {props.loggedInStatus === "LOGGED_IN" ? <a onClick={handleSignOut}>Sign Out</a>: null }
-        </div>
+        {props.loggedInStatus === "LOGGED_IN" ? (
+          <a onClick={handleSignOut}>
+            <FontAwesomeIcon icon="sign-out-alt" />
+          </a>
+        ) : null}
+      </div>
     </div>
   );
 };
